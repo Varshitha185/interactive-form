02db5c03a9a3bc50dda0c0f6438704775e925cb3
Varshitha C <varshithac185@gmail.com>
2026-04-16 21:55:43 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,11 +3,15 @@ function sample() {
     const email = document.getElementById("email").value;
     const message = document.getElementById("message").value;
 
-    // store in localStorage
-    localStorage.setItem("name", name);
-    localStorage.setItem("email", email);
-    localStorage.setItem("message", message);
+    // get existing data OR empty array
+    let submissions = JSON.parse(localStorage.getItem("submissions")) || [];
 
-    // go to next page
+    // add new entry
+    submissions.push({ name, email, message });
+
+    // save back
+    localStorage.setItem("submissions", JSON.stringify(submissions));
+
+    // go to view page
     window.location.href = "view.html";
 }
